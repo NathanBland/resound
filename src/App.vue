3789828e50940d55cbf89446cf33495a125b1c9d
Nathan Bland <nathan.bland@gmail.com>
2016-05-13 13:55:06 -0600

    Starting point

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang='jade'> 
+#app
+  test
+  p Blank slate
+</template>
+
+<script>
+import Test from './components/Test'
+
+export default {
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,25 @@
+<template lang='jade'>
+  .hello
+    h1 {{msg}}
+    input(v-model="msg")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='sass'>
+h1
+  color: #42b983
+</style>
